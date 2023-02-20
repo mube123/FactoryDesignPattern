e09d1b93357fd1d188c8a4296fa332fbf3b2f57b
Mubeen Hameed <rabimubeen955@gmail.com>
2023-02-20 20:13:50 +0500

    Initial commit

---
+++ src/factory/design/pattern/InstitutionalPlan.java
@@ -0,0 +1,9 @@
+package factory.design.pattern;
+
+class  InstitutionalPlan extends Plan{  
+	   //@override  
+	    public void getRate(){   
+	        rate=5.50;  
+	   }   
+}//end of InstitutionalPlan class.
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mubeen
+ *
+ */
+module FactoryDesignPattern {
+}
